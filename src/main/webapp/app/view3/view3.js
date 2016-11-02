5bbb025e9fa172d945ecfeeaedc584dd1d3faa81
josephawwal <josephawwal@192.168.1.4>
2016-11-02 11:10:48 +0100

    rest assured

--- src/main/webapp/app/view3/view3.js
+++ src/main/webapp/app/view3/view3.js
@@ -3,19 +3,42 @@
 angular.module('myApp.view3', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/companyInfo', {
+  $routeProvider.when('/view3', {
     templateUrl: 'app/view3/view3.html',
     controller: 'View3Ctrl'
   });
 }])
-
+    
 .controller('View3Ctrl', function($http,$scope) {
-  $http.get('api/demoadmin')
-            .success(function (data, status, headers, config) {
-              $scope.data = data;
-            })
-            .error(function (data, status, headers, config) {
-              
-             });
+  $scope.searchInput;
+  $scope.resultList;
+  $scope.isPopulated = false;
+  $scope.searchType = "search";
+  $scope.searchTypeHeading;
+  
+  $scope.setSearchType = function(searchType, searchTypeHeading){
+      $scope.searchType = searchType;
+      $scope.searchTypeHeading = searchTypeHeading;
+      
+  };
+  
+  $scope.search = function (){
+      if ($scope.searchInput === undefined || $scope.searchInput === ""){
+          alert("Input Field Empty");
+          return;
+      }
+      $http({
+          method: 'GET',
+          dataType: 'json',
+          url: 'http://cvrapi.dk/api?'+ $scope.searchType + '=' + $scope.searchInput.toLowerCase() + '&country=dk',
+          skipAuthorization: true
+      }).then(function (response){
+          $scope.resultList = response.data;
+          $scope.isPopulated = true;
+      }, function (response){
+          alert(response.data.error);
+          $scope.isPopulated = false;
+      });
+  };
  
 });
