900c7fde62973b882c30dda90dc1a7ed5cbb70cd
olls <olls@HenrikDesktop>
2016-11-03 17:24:01 +0100

    Moved tests to test package

--- src/test/java/backendTests.java
+++ src/test/java/backendTests.java
@@ -4,8 +4,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
 import static com.jayway.restassured.RestAssured.*;
 import com.jayway.restassured.parsing.Parser;
 import facades.UserFacade;
@@ -31,16 +29,17 @@ import static org.junit.Assert.assertTrue;
  */
 public class backendTests
 {
+
     static Server server;
     public EntityManagerFactory emf = Persistence.createEntityManagerFactory("seedPU");
-    
+
     public backendTests()
     {
-        baseURI="http://localhost:8082";
+        baseURI = "http://localhost:8089";
         defaultParser = Parser.JSON;
-        basePath ="/api";
+        basePath = "/api";
     }
-    
+
     @BeforeClass
     public static void setUpClass() throws Exception
     {
@@ -53,24 +52,24 @@ public class backendTests
         server.setHandler(contextHandler);
         server.start();
     }
-    
+
     @AfterClass
     public static void tearDownClass() throws Exception
     {
         server.stop();
         server.join();
     }
-    
+
     @Before
     public void setUp()
     {
-        
+
     }
-    
+
     @After
     public void tearDown()
     {
-        
+
     }
 
     @Test
@@ -78,13 +77,36 @@ public class backendTests
     {
         EntityManager em = emf.createEntityManager();
         UserFacade uf = (UserFacade) security.UserFacadeFactory.getInstance();
-        User usr1 = new User("John","Arne");
-        int id = usr1.getId();
+        User usr1 = new User("John", "Arne");
         uf.addUser(usr1);
+        int id = usr1.getId();
         assertTrue(em.find(User.class, id).getUserName().equals("John"));
     }
-    
-    
+
+    @Test
+    public void test2()
+    {
+        given().
+                contentType("application/json").
+                body("{'username':'jegerikkeenbruger','password':'test'}").
+                when().
+                post("/login").
+                then().
+                statusCode(401);
+    }
+
+    @Test
+    public void test3()
+    {
+        given().
+                contentType("application/json").
+                body("{'username':'user','password':'test'}").
+                when().
+                post("/login").
+                then().
+                statusCode(200);
+    }
+
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
     //
