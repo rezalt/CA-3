c202c28da5688718e007635a8b9d9288f583f6b3
josephawwal <josephawwal@192.168.1.4>
2016-11-05 11:39:36 +0100

    rest assured

--- src/main/java/rest/CurrencyRest.java
+++ src/main/java/rest/CurrencyRest.java
@@ -8,29 +8,30 @@ package rest;
 import com.google.gson.Gson;
 import entity.DailyRate;
 import facades.CurrencyFacade;
-import facades.CurrencyFacade;
 import java.util.List;
 import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 /**
  *
  * @author josephawwal
  */
 @RolesAllowed("User")
 @Path("currency/dailyrates")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+
 public class CurrencyRest {
     
     
     @Context
     private UriInfo context;
     private CurrencyFacade cf = new CurrencyFacade();
-    private Gson gson = new Gson();
     
     public CurrencyRest(){
         
@@ -38,9 +39,11 @@ public class CurrencyRest {
     
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getDailyRates(){
+    public Object getDailyRates(){
         List<DailyRate> rates = cf.getDailyRates();
-        return Response.ok(gson.toJson(rates)).build();
+        Gson gson = new Gson();
+        return new Gson().toJson(rates);
+        
     }
     
     
