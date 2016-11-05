420156ba5ac275ab8d008334f43a92a6646ef7b0
josephawwal <josephawwal@192.168.1.4>
2016-11-05 13:18:18 +0100

    rest assured

--- src/main/java/facades/CurrencyFacade.java
+++ src/main/java/facades/CurrencyFacade.java
@@ -7,18 +7,59 @@ package facades;
 import data.CurrencyNB;
 import entity.DailyRate;
 import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
 /**
  *
  * @author josephawwal
  */
 public class CurrencyFacade {
     
-    public CurrencyFacade(){
+    EntityManagerFactory emf;
+    
+    
+    public CurrencyFacade(EntityManagerFactory emf){
+        this.emf = emf;
         
     }
     
+    private EntityManager getEntityManager(){
+        return emf.createEntityManager();
+    }
+    public void addEntityManagerFactory(EntityManagerFactory emf){
+        this.emf = emf;
+    }
+    
+    
+    
+    
     public List<DailyRate> getDailyRates(){
-        return CurrencyNB.getDailyRates();
+        EntityManager em = getEntityManager();
+        try {
+            Query query = em.createQuery(""); //SELECT e FROM rate E
+            List<DailyRate> rates = query.getResultList();
+            if (rates != null){
+                System.out.println("Rates:" + rates.size());
+                
+            } else {
+                System.out.println("null");
+            }
+            return rates;
+        } finally {
+        em.close();
+    }
+    }
+    
+    public DailyRate getRateByCurrency(String currency){
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(DailyRate.class, currency);
+            
+        } finally {
+            em.close();
+        }
+    
     }
     
 }
