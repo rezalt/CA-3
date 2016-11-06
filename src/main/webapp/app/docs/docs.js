4355703cbbd491e9315ca2396e1790a10c77dd49
olls <olls@HenrikDesktop>
2016-11-06 17:48:10 +0100

    Done with John project Arne mand.

--- src/main/webapp/app/docs/docs.js
+++ src/main/webapp/app/docs/docs.js
@@ -5,13 +5,15 @@ angular.module('myApp.docs', ['ngRoute'])
         .config(['$routeProvider', function ($routeProvider) {
             $routeProvider.when('/docs', {
               templateUrl: 'app/docs/docs.html',
-              controller: 'View2Ctrl'
+              controller: 'View2Ctrl',
+              controllerAs: "ctrl"
             });
           }])
       
       
 
-        .controller('View2Ctrl', function ($http, $scope) {
+        .controller('View2Ctrl', function () {
+        
          
-
+         
         });
