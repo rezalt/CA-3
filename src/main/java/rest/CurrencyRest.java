14b14128a8a2834dc927b909f0e9f147fd93d4f3
olls <olls@HenrikDesktop>
2016-11-06 10:01:13 +0100

    Added @RolesAllowed("User") to CurrencyRest.

--- src/main/java/rest/CurrencyRest.java
+++ src/main/java/rest/CurrencyRest.java
@@ -24,6 +24,7 @@ import javax.ws.rs.core.Response;
  * @author josephawwal
  */
 @Path("currency")
+@RolesAllowed("User")
 public class CurrencyRest
 {
     private static Gson gson = new GsonBuilder().setPrettyPrinting().create();
