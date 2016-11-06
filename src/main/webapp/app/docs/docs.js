b79c6d32e70ad9030fd6cb7b67ac2a08750eddf3
olls <olls@HenrikDesktop>
2016-11-06 13:36:24 +0100

    Docs page is ready with working tabs for each subject.
    Needs to be made pretty.

--- src/main/webapp/app/docs/docs.js
+++ src/main/webapp/app/docs/docs.js
@@ -8,15 +8,10 @@ angular.module('myApp.docs', ['ngRoute'])
               controller: 'View2Ctrl'
             });
           }])
+      
+      
 
         .controller('View2Ctrl', function ($http, $scope) {
-          $http({
-            method: 'GET',
-            url: 'api/demouser'
-          }).then(function successCallback(res) {
-            $scope.data = res.data.message;
-          }, function errorCallback(res) {
-            $scope.error = res.status + ": "+ res.data.statusText;
-          });
+         
 
         });
